a08ac14a123b1fa8485f85dd1bd83c8107d3f14d
Prajwal Joshi <prazwal@outlook.com>
2016-04-16 13:46:44 +1000

    annotate models

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: reservations
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer  
+#  location     :text
+#  trade_name   :text
+#  request_time :datetime
+#  comments     :text
+#  job_status   :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Reservation < ActiveRecord::Base
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  tradie_id  :integer
+#  comment    :text
+#  rating     :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Review < ActiveRecord::Base
+  
 end
