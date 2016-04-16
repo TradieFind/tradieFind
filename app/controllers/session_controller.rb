5f519ebf8b448c4e4474ad97610f40eefff503b5
Prajwal Joshi <prazwal@outlook.com>
2016-04-16 19:02:23 +1000

    do login, sign up, navbar

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,21 +3,38 @@ class SessionController < ApplicationController
   def new
   end
 
+  #def create
+  #  user = User.find_by :email => params[:email]
+  #    if user.present? && user.authenticate(params[:password])
+  #      session[:user_id] = user.id
+  #      ##log em in
+  #      redirect_to root_path
+  #    else
+  #      flash[:error] = "Incorrect password or email"
+  #      redirect_to login_path
+  #  end
+  # end
+  #
+  # def destroy
+  #  session[:user_id] = nil
+  #  redirect_to root_path
+
   def create
-   user = User.find_by :email => params[:email]
-     if user.present? && user.authenticate(params[:password])
-       session[:user_id] = user.id
-       #log em in
-       redirect_to root_path
-     else
-       flash[:error] = "Incorrect password or email"
-       redirect_to login_path
-   end
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in."
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "Invalid Email and/or Password!"
+        render 'new'
+    end
   end
 
   def destroy
-   session[:user_id] = nil
-   redirect_to root_path
+    session[:user_id] = nil
+    flash[:success] = "You have logged out"
+    redirect_to root_path
   end
 
 end
