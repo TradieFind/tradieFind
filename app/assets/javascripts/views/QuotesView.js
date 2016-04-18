d9e2e34142f9dd176648326650d2f656ce211b37
mgundies <mgundies@mgundiess-MacBook-Pro.local>
2016-04-18 15:54:06 +1000

    added current user

--- app/assets/javascripts/views/QuotesView.js
+++ app/assets/javascripts/views/QuotesView.js
@@ -12,8 +12,8 @@ app.QuotesView = Backbone.View.extend({
     // var appViewTemplate = $("#anotherTemplate").html();
     // this.$el.append(appViewTemplate);
     var addData = this.$el.find("#quoteData");
-
-    app.quotes.each(function(q){
+    var quoteForTradie = app.quotes.where({user_id: app.current_user.id});
+    quoteForTradie.each(function(q){
       console.log(q);
       var x = $('<li>').text(q.get("id") + " reservation_id: " + q.get("reservation_id") + " quote_value: " + q.get("quote_value"));
       addData.after(x);
@@ -21,3 +21,20 @@ app.QuotesView = Backbone.View.extend({
   }
 
 });
+
+
+// confirmSeat: function() {
+//      if ($(".selected").length > 0) {
+//          clearInterval(checkForReservation);
+//          var flight_id = this.model.attributes.id;
+//          var seat = $(".selected").eq(0).attr("id");
+//          var reservation = app.reservations.where({user_id: app.current_user.id, seat: seat, flight_id: flight_id });
+//          app.router.navigate('reservations/' + reservation[0].id, true);
+//
+//        // var userID = @current_user.id;
+//
+//      } else {
+//          alert("Please select a seat.");
+//          return;
+//      }
+//  }
