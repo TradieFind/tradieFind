f40980cd5968092e15d7a8a7ae8ce07134cb8132
Prajwal Joshi <prazwal@outlook.com>
2016-04-19 12:48:49 +1000

    fix trades dropdown

--- app/assets/javascripts/views/TradeView.js
+++ app/assets/javascripts/views/TradeView.js
@@ -2,12 +2,13 @@ var app = app || {};
 
 app.TradeView = Backbone.View.extend({
   initialize:function(){
-    console.log("here");
+
   },
   tagName: "option",
   render: function(){
     var name = this.model.get("name");
     this.$el.text(name);
-    this.$el.appendTo("#options");
+    console.log(name);
+    this.$el.appendTo('#tradeOptions');
   }
 });
