ed2265304207e221e0cda31b5a3fcdf43d5c1324
Jay Byun <jaybyun@Macintosh.local>
2016-04-19 12:26:42 +1000

    footer homeContent show/hide

--- app/assets/javascripts/routers/AppRouter.js
+++ app/assets/javascripts/routers/AppRouter.js
@@ -16,6 +16,7 @@ app.AppRouter = Backbone.Router.extend({
     home: function() {
       var appView = new app.AppView();
       appView.render();
+      $(".homeIndexContent").show();
     },
 
     showReservation: function(id) {
@@ -23,12 +24,14 @@ app.AppRouter = Backbone.Router.extend({
       var reservationView = new app.ReservationView({model: reservation});
       app.reservation_id = id;
       reservationView.render();
+      $(".homeIndexContent").hide();
     },
 
     showConfirmed: function() {
       var reservation = app.reservations.get(app.reservation_id);
       var confirmationView = new app.ConfirmationView({ model: reservation });
       ConfirmationView.render();
+      $(".homeIndexContent").hide();
     },
 
     showUser: function(id) {
@@ -36,6 +39,7 @@ app.AppRouter = Backbone.Router.extend({
       var userView = new app.UserView({model: user});
       app.user_id = id;
       userView.render();
+      $(".homeIndexContent").hide();
     },
 
     reviewInput: function() {
@@ -43,6 +47,7 @@ app.AppRouter = Backbone.Router.extend({
       var review = user.review;
       var reviewInputView = new app.ReviewInputView({ model: review });
       reviewInputView.render();
+      $(".homeIndexContent").hide();
     },
 
     showReview: function(id) {
@@ -50,22 +55,26 @@ app.AppRouter = Backbone.Router.extend({
       var reviewView = new app.ReviewView({model: review});
       app.user_id = id;
       reviewView.render();
+      $(".homeIndexContent").hide();
     },
 
     showQuotes: function() {
       console.log("showQuotes: function()");
       var quoteView = new app.QuotesView();
       quoteView.render();
+      $(".homeIndexContent").hide();
     },
 
     contactUs: function() {
       var contactUsView = new app.contactUsView();
       contactUsView.render();
+      $(".homeIndexContent").hide();
     },
 
     faq: function() {
       var faqView = new app.FaqView();
       faqView.render();
+      $(".homeIndexContent").hide();
     }
 
   });
