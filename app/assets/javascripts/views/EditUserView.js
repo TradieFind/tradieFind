1e0ce8a13f3b317df857c059284f071649530727
Prateek Obireddy <Prateek@192-168-1-3.tpgi.com.au>
2016-04-20 21:19:55 +1000

    Bootstrapped edit user profile

--- app/assets/javascripts/views/EditUserView.js
+++ app/assets/javascripts/views/EditUserView.js
@@ -15,41 +15,41 @@ $(document).ready(function(){
 
   saveUser: function(){
     var user = app.users.get(app.current_user);
-    var userFirstNameFromHTML = this.$el.find("#first_name").val();
-       user.set({first_name: userFirstNameFromHTML});
+    // var userFirstNameFromHTML = this.$el.find("#first_name").val();
+    //    user.set({first_name: userFirstNameFromHTML});
+    //
+    //
+    // var userLastNameFromHTML = this.$el.find("#last_name").val();
+    //   user.set({last_name: userLastNameFromHTML});
+    //
+    //
+    // var userEmailFromHTML = this.$el.find("#email").val();
+    //   user.set({email: userEmailFromHTML});
+    //
+    //
+    // var userCompanyNameFromHTML = this.$el.find("#company_name").val();
+    //   user.set({company_name: userCompanyNameFromHTML});
+    //
+    //
+    // var userTradeFromHTML = this.$el.find("#trade").val();
+    //   user.set({trade: userTradeFromHTML});
+    //
+    //
+    // var userRateFromHTML = this.$el.find("#rate").val();
+    //   user.set({rate: userRateFromHTML});
+    //
+    //
+    // var userPhoneNumberHTML = this.$el.find("#phone_no").val();
+    //   user.set({phone_no: userPhoneNumberHTML});
+    //
+    //
+    // var userQualificationsHTML = this.$el.find("#qualifications").val();
+    //   user.set({qualifications: userQualificationsHTML});
+    //
+    //
+    // var userAddressOneHTML = this.$el.find("#address_one").val();
+    //   user.set({address_one: userAddressOneHTML});
 
-
-    var userLastNameFromHTML = this.$el.find("#last_name").val();
-      user.set({last_name: userLastNameFromHTML});
-      
-
-    var userEmailFromHTML = this.$el.find("#email").val();
-      user.set({email: userEmailFromHTML});
-      
-
-    var userCompanyNameFromHTML = this.$el.find("#company_name").val();
-      user.set({company_name: userCompanyNameFromHTML});
-      
-
-    var userTradeFromHTML = this.$el.find("#trade").val();
-      user.set({trade: userTradeFromHTML});
-      
-
-    var userRateFromHTML = this.$el.find("#rate").val();
-      user.set({rate: userRateFromHTML});
-      
-
-    var userPhoneNumberHTML = this.$el.find("#phone_no").val();
-      user.set({phone_no: userPhoneNumberHTML});
-      
-
-    var userQualificationsHTML = this.$el.find("#qualifications").val();
-      user.set({qualifications: userQualificationsHTML});
-      
-
-    var userAddressOneHTML = this.$el.find("#address_one").val();
-      user.set({address_one: userAddressOneHTML});
-      
 
     var userAddressTwoHTML = this.$el.find("#address_two").val();
       user.set({address_two: userAddressTwoHTML});
