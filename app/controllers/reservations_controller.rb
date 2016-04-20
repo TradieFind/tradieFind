922c9993cbacdb4ae5cc2b52b9b2e66606e21b21
Prajwal Joshi <prazwal@outlook.com>
2016-04-20 16:56:12 +1000

    add quote_id in reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -69,6 +69,6 @@ class ReservationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def reservation_params
-      params.require(:reservation).permit(:user_id, :location, :trade_name, :request_time, :comments, :job_status)
+      params.require(:reservation).permit(:user_id, :location, :quote_id, :trade_name, :request_time, :comments, :job_status)
     end
 end
