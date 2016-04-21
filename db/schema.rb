108c4a906f1f7a32b4b94306916451b4fa0140a6
Prajwal Joshi <prazwal@outlook.com>
2016-04-21 17:58:31 +1000

    more changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160421035746) do
+ActiveRecord::Schema.define(version: 20160421061137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,10 +58,6 @@ ActiveRecord::Schema.define(version: 20160421035746) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
     t.string   "email"
     t.text     "password_digest"
     t.string   "company_name"
@@ -74,8 +69,8 @@ ActiveRecord::Schema.define(version: 20160421035746) do
     t.string   "address_two"
     t.float    "lat"
     t.float    "lon"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "avatar"
   end
 
